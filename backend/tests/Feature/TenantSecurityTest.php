3bd202e722c115dce295cacad19092e384b5484a
Leo-o-nardo <leonardo@infinitetecnologia.com.br>
2025-12-31 09:22:34 -0300

    Laravel test corrections

--- backend/tests/Feature/TenantSecurityTest.php
+++ backend/tests/Feature/TenantSecurityTest.php
@@ -7,9 +7,11 @@ use App\Models\Tenant;
 use App\Models\Sensor;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TenantSecurityTest extends TestCase
 {
+    use RefreshDatabase;
     use DatabaseTransactions;
 
     public function test_tenant_can_only_see_their_own_sensors()
